e379f58e626b570c22f11fa083f3f101dcd0e8eb
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2022-02-25 23:16:36 +0300

    lab1 добавил 15 задание

---
+++ lab1/task15/down.sql
@@ -0,0 +1,7 @@
+USE `task15`;
+DROP TABLE `ticket`;
+DROP TABLE `transport`;
+DROP TABLE `passenger`;
+DROP TABLE `route`;
+DROP TABLE `transport_node`;
+DROP DATABASE `task15`;
---
+++ lab1/task15/up.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE IF NOT EXISTS `task15`;
+
+USE `task15`;
+
+CREATE TABLE IF NOT EXISTS `transport_node` (
+    `transport_node_id` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `location` VARCHAR(255) NOT NULL,
+    `type` TINYINT NOT NULL DEFAULT 1,
+    PRIMARY KEY (`transport_node_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `route` (
+    `route_id` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `starting_node_id` INT NOT NULL,
+    `destination_node_id` INT NOT NULL,
+    `qualification` TINYINT NOT NULL,
+    `birth_date` DATE NOT NULL,
+    PRIMARY KEY (`route_id`),
+    FOREIGN KEY (`starting_node_id`) REFERENCES `transport_node`(`transport_node_id`),
+    FOREIGN KEY (`destination_node_id`) REFERENCES `transport_node`(`transport_node_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `passenger` (
+    `passenger_id` INT NOT NULL,
+    `first_name` VARCHAR(255) NOT NULL,
+    `middle_name` VARCHAR(255),
+    `last_name` VARCHAR(255) NOT NULL,
+    `document_type` TINYINT NOT NULL,
+    `document_number` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`passenger_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `transport` (
+    `transport_id` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `type` TINYINT NOT NULL DEFAULT 1,
+    `current_route_id` INT,
+    `status` TINYINT,
+    PRIMARY KEY (`transport_id`),
+    FOREIGN KEY (`current_route_id`) REFERENCES `route`(`route_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ticket` (
+    `ticket_id` INT NOT NULL,
+    `price` INT NOT NULL,
+    `currency` CHAR(3) NOT NULL,
+    `passenger_id` INT NOT NULL,
+    `route_id` INT NOT NULL,
+    `transport_id` INT NOT NULL,
+    `datetime_at` DATETIME NOT NULL,
+    PRIMARY KEY (`ticket_id`)
+);
